84f46580d3d14f2d565be6cb38ff528ed1af08e1
yusuf <yusufgad24@gmail.com>
2024-03-16 17:34:41 +0300

    Add productRouter and update routes in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,19 @@
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
+import productRouter from "./routes/productRouter.js";
 import connectDB from "./config/db.js";
-
-import products from "./data/products.js";
 
 const PORT = process.env.PORT || 8000;
 
 connectDB();
 const app = express();
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
+app.get("/", (req, res) => {
+  res.send("API is running...");
 });
 
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+app.use("/api/products", productRouter);
 
 app.listen(PORT, () => {
   console.log("Server started at port " + PORT);
