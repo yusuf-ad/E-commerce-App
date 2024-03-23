ea1b74ab412ab877095c2eff6e4b00cf4908e912
yusuf <yusufgad24@gmail.com>
2024-03-23 09:52:07 +0300

    Add LoginScreen component and authSlice to store

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -19,6 +19,7 @@ import CartScreen from "./screens/CartScreen.jsx";
 // for Redux
 import { Provider } from "react-redux";
 import store from "./store.js";
+import LoginScreen from "./screens/LoginScreen.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -26,6 +27,7 @@ const router = createBrowserRouter(
       <Route index={true} path="/" element={<HomeScreen />} />
       <Route path="/product/:id" element={<ProductScreen />} />
       <Route path="/cart" element={<CartScreen />} />
+      <Route path="/login" element={<LoginScreen />} />
     </Route>
   )
 );
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,12 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from "./slices/apiSlice";
 import cartSliceReducer from "./slices/cartSlice";
+import authSliceReducer from "./slices/authSlice";
 
 const store = configureStore({
   reducer: {
     // Add the generated apiSlice reducer to the store
     [apiSlice.reducerPath]: apiSlice.reducer,
     cart: cartSliceReducer,
+    auth: authSliceReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(apiSlice.middleware),
