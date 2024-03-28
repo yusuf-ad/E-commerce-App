c7343017f93597be984d6ab62cd91d519544cd20
yusuf <yusufgad24@gmail.com>
2024-03-28 19:43:56 +0300

    Refactor order routes and controller

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -5,21 +5,67 @@ import Order from "../models/orderModel.js";
 // @route   POST /api/order
 // @access  Private
 export const addOrderItems = asyncHandler(async (req, res) => {
+  const {
+    orderItems,
+    shippingAddress,
+    paymentMethod,
+    itemsPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+  } = req.body;
+
+  if (orderItems && orderItems.length === 0) {
+    res.status(400);
+    throw new Error("No order items");
+  } else {
+    const order = new Order({
+      orderItems: orderItems.map((item) => ({
+        ...item,
+        product: item._id,
+        ref: "Product",
+      })),
+      user: req.user._id,
+      shippingAddress,
+      paymentMethod,
+      itemsPrice,
+      taxPrice,
+      shippingPrice,
+      totalPrice,
+    });
+
+    const createdOrder = await order.save();
+
+    res.status(201).json(createdOrder);
+  }
+
   res.send("add order items");
 });
 
 // @desc    Get logged in user orders
-// @route   GET /api/order/mine
+// @route   GET /api/order/myorders
 // @access  Private
 export const getMyOrders = asyncHandler(async (req, res) => {
-  res.send("get my orders");
+  const orders = await Order.find({ user: req.user._id });
+
+  res.status(200).json(orders);
 });
 
 // @desc    Get order by ID
 // @route   GET /api/orders/:id
 // @access  Private/Admin
 export const getOrderById = asyncHandler(async (req, res) => {
-  res.send("get order by id");
+  const order = await Order.findById(req.params.id).populate(
+    "user",
+    "name email"
+  );
+
+  if (order) {
+    res.status(200).json(order);
+  } else {
+    res.status(404);
+    throw new Error("Order not found");
+  }
 });
 
 // @desc    Update order to paid
--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -14,7 +14,7 @@ const router = express.Router();
 
 router.route("/").post(protect, addOrderItems).get(protect, admin, getOrders);
 
-router.route("/mine").get(protect, getMyOrders);
+router.route("/myorders").get(protect, getMyOrders);
 
 router.route("/:id").get(protect, getOrderById);
 router.route("/:id/pay").put(protect, updateOrderToPaid);
